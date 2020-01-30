30159d96eceba102808be51c1c567d510f9d8438
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-30 01:47:53 -0300

    cloudinary was created to add photo

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Signup from "./components/auth/signup";
+import AddThing from './components/AddPhoto';
+import AddPhoto from './components/AddPhoto';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { loggedInUser: null };
- 
+
 
   }
   render() {
-    
+
     return (
       <div className="App">
-          <h1>I cat your pet</h1>
-          
+        <h1>I cat your pet</h1>
+
         <Switch>
-            <Route
-              exact
-              path="/signup"
-              user={this.state.loggedInUser}
-              render={props => <Signup getUser={this.getTheUser} {...props} />}
-            />
+          <Route
+            exact
+            path="/signup"
+            user={this.state.loggedInUser}
+            render={props => <Signup getUser={this.getTheUser} {...props} />}
+          />
         </Switch>
+
+        <div className="App">
+          <AddPhoto />
+        </div>
 
       </div>
     )
