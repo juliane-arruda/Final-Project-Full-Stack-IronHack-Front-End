887896510f1b3064414817e39cf36c4f71c85cc8
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-30 18:57:46 -0300

    Co-authored-by: Vinicius Melquiades <viniciusmelquiades@gmail.com>

--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -2,56 +2,55 @@ import axios from 'axios';
 
 class AuthService {
 
-    constructor() {
-        this.service = axios.create({
-            baseURL: process.env.REACT_APP_API,
-            withCredentials: true
-        })
-    }
-    
-    signup(username, password, email, role, petName, petDescription, imageUrl, petLocation, petDate) {
-        return this.service.post('/signup', {
-            username,
-            password,
-            email,
-            role,
-            petName,
-            petDescription,
-            imageUrl,
-            petLocation,
-            petDate,
-        })
-        .then(response => response.data)
-    }
+  constructor() {
+    this.service = axios.create({
+      baseURL: process.env.REACT_APP_API,
+      withCredentials: true
+    })
+  }
 
-    handleUpload(theFile) {
-        // console.log('file in service: ', theFile)
-        return this.service.post('/upload', theFile)
-          .then(res => res.data)
-          .catch(error => console.log(error));
-      }
+  signup(username, password, email, role, petName, petDescription, imageUrl, petLocation, petDate) {
+    return this.service.post('/signup', {
+        username,
+        password,
+        email,
+        role,
+        petName,
+        petDescription,
+        imageUrl,
+        petLocation,
+        petDate,
+      })
+      .then(response => response.data)
+  }
 
-    login(username, password) {
-        return this.service.post('/login', {
-            username,
-            password
-        })
-        .then(response => response.data)
-        }
+  handleUpload(theFile) {
+    // console.log('file in service: ', theFile)
+    return this.service.post('/upload', theFile)
+      .then(res => res.data)
+      .catch(error => console.log(error));
+  }
+
+  login(username, password) {
+    return this.service.post('/login', {
+        username,
+        password
+      })
+      .then(response => response.data)
+  }
 
 
 
-    AddPet(petName, petDescription, imageUrl, petLocation, petDate) {
-        return this.service.post('/add-pet', {
-            petName,
-            petDescription,
-            imageUrl,
-            petLocation,
-            petDate
-            })
-            .then(response => response.data)
-        }
-
+  AddPet(petName, petDescription, imageUrl, petLocation, petDate) {
+    return this.service.post('/add-pet', {
+        petName,
+        petDescription,
+        imageUrl,
+        petLocation,
+        petDate
+      })
+      .then(response => response.data)
+  }
+}
 
 export default AuthService;
-
