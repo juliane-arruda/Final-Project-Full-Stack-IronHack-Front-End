0e1375c4e06725b8426add11e267ed133040295f
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-30 15:01:44 -0300

    organizing photo routes

--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
 class AuthService {
-    constructor() {
-        this.service = axios.create({
-            baseURL: process.env.REACT_APP_API,
-            withCredentials: true
-        })
-    }
-    
-    signup(username, password, email, role) {
-        return this.service.post('/signup', {
-            username,
-            password,
-            email,
-            role
-        })
-        .then(response => response.data)
-    }
+  constructor() {
+    this.service = axios.create({
+      baseURL: process.env.REACT_APP_API,
+      withCredentials: true
+    })
+  }
 
-    login(username, password) {
-        return this.service.post('/login', {
-            username,
-            password
-          })
-          .then(response => response.data)
-      }
+  signup(username, password, email, role) {
+    return this.service.post('/signup', {
+        username,
+        password,
+        email,
+        role
+      })
+      .then(response => response.data)
+  }
+
+  login(username, password) {
+    return this.service.post('/login', {
+        username,
+        password
+      })
+      .then(response => response.data)
+  }
 
 }
 
