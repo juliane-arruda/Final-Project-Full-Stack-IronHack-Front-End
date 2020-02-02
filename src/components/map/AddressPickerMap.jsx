f0f28268749dd32cf2819d031c44dd53e8643503
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-02 14:14:57 -0300

    added map to add address

---
+++ src/components/map/AddressPickerMap.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  GoogleMap,
+  withGoogleMap,
+  Marker,
+} from 'react-google-maps';
+import { LoadGoogleMapsJs } from './LoadGoogleMapsJs';
+import { MapContainer } from './MapContainer';
+// import './DisplayMap.scss';
+
+// const googleMapStylerNoVisibility = [{ visibility: 'off' }];
+const googleMapStylerNoVisibility = [{ visibility: 'on' }];
+
+
+class AddessMapBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  state = {
+    position: null,
+  }
+
+  onClick(e) {
+    const position = e.latLng.toJSON();
+
+    this.setState({
+      position,
+    });
+
+    const { onPositionChanged } = this.props;
+    typeof onPositionChanged === 'function' && onPositionChanged(position)
+  }
+
+  render() {
+    return (
+      <GoogleMap
+        {...this.props}
+        onClick={this.onClick}
+        options={{
+          clickableIcons: false,
+          fullscreenControl: false,
+          mapTypeControl: false,
+          disableDefaultUI: false,
+          streetViewControl: false,
+          styles: [
+            {
+              featureType: 'transit',
+              stylers: this.googleMapStylerNoVisibility,
+            },
+            { featureType: 'poi', stylers: this.googleMapStylerNoVisibility },
+          ],
+        }}
+      >
+        {this.state.position && <Marker position={this.state.position} />}
+      </GoogleMap>
+    );
+  }
+}
+
+const AddressMapAnnotated = withGoogleMap(AddessMapBase);
+
+export const AddressMap =
+  ({
+    loadingElement,
+    ...props
+  }) => {
+    return (
+      <LoadGoogleMapsJs loadingElement={loadingElement}>
+        <AddressMapAnnotated {...props}
+          containerElement={MapContainer()} />
+      </LoadGoogleMapsJs>
+    );
+  };
---
+++ src/components/map/MapContainer.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './MapContainer.css';
+
+export const MapContainer = () => <div className="mapContainer"></div>;
