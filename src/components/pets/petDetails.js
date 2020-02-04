b066f045267a50d9299e4f20d779b18962cb5030
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-04 15:42:09 -0300

    fixed module to track pet status

--- src/components/pets/petDetails.js
+++ src/components/pets/petDetails.js
@@ -5,75 +5,79 @@ import { Link } from "react-router-dom";
 
 
 class PetDetails extends Component {
-    constructor(props) {
-        // console.log(props)
-        super(props);
-        this.state = {};
-        this.getSinglePet = this.getSinglePet.bind(this);
-        this.deletePet = this.deletePet.bind(this);
-    }
-    componentDidMount() {
-        this.getSinglePet()
-      }
-    
-      getSinglePet() {
-        const { params } = this.props.match;
-        axios
-          .get(`${process.env.REACT_APP_API}pets/${params.id}`, {
-          })
-          .then(responseFromApi => {
-            const pet = responseFromApi.data;
-            this.setState(pet);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
+  constructor(props) {
+    // console.log(props)
+    super(props);
+    this.state = {};
+    this.getSinglePet = this.getSinglePet.bind(this);
+    this.deletePet = this.deletePet.bind(this);
+  }
+  componentDidMount() {
+    this.getSinglePet()
+  }
 
-      deletePet() {
-        const { params } = this.props.match;
-        console.log(params.id,"id")
-          axios
-          .delete(`${process.env.REACT_APP_API}pets/${params.id}`, {
-          })
-          .then(() => {
-            this.props.history.push("/pets");
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
+  getSinglePet() {
+    const { params } = this.props.match;
+    axios
+      .get(`${process.env.REACT_APP_API}pets/${params.id}`, {
+      })
+      .then(responseFromApi => {
+        const pet = responseFromApi.data;
+        this.setState(pet);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-      render() {
-        return (
-          <div>
-            <h1> {this.state.petName} </h1> 
-            <p> {this.state.petDescription} </p>
-            <p> Data que foi {this.state.role}: {this.state.petDate} </p>
-            <p>{this.state.role}</p>
-            <p> {} </p>
+  deletePet() {
+    const { params } = this.props.match;
+    console.log(params.id, "id")
+    axios
+      .delete(`${process.env.REACT_APP_API}pets/${params.id}`, {
+      })
+      .then(() => {
+        this.props.history.push("/pets");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-            <img src={this.state.imageUrl}/>
-                    <button onClick={this.deletePet}>
-                      Apagar pet
-                    </button>
-                    <Link to={`/pets/${this.props.match.params.id}/edit`}>
-                      Editar pet
-                    </Link>
-                    
-            
-                {/* {this.props.loggedInUser &&
-                  this.props.loggedInUser._id === pet.owner && (
-                )} */}
-
-            <Link to={"/pets"}> Back to pets </Link>
-
-
+  render() {
+    return (
+      <div className="container">
+        <h1 className="pt-5 col-12">Detalhe do Seu Pet</h1>
+        <div className="d-flex">
+          <div className="container">
+            <img src={this.state.imageUrl} />
           </div>
-        )
-      }
-    }
-    
-    
+          <div className="container">
+            <h4>Nome: {this.state.petName} </h4>
+            <p>Descrição: {this.state.petDescription} </p>
+            <p>Data que foi: {this.state.role}: {this.state.petDate} </p>
+            <p>Estado: {this.state.role}</p>
+            <p>Endereço: {} </p>
+
+            <div className="col-12">
+            <Link className="btn btn-danger m-1" onClick={this.deletePet}>
+              Apagar pet
+            </Link>
+            <Link className="btn btn-info m-1" to={`/pets/${this.props.match.params.id}/edit`}>
+              Editar pet
+            </Link>
+            {/* {this.props.loggedInUser && this.props.loggedInUser._id === pet.owner && ()} */}
+            <Link className
+            ="btn btn-secondary m-1" to={"/pets"}> Back to pets </Link>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
 
 export default PetDetails;
