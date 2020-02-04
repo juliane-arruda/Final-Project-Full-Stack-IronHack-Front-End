97368f8389b6aaf348cef51107d90d60053d2f45
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-04 20:19:52 -0300

    done the first version of the login layout

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import AuthService from './auth-service';
 import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { username: "", password: ""};
-        this.service = new AuthService();
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" };
+    this.service = new AuthService();
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-handleFormSubmit(event) {
+  handleFormSubmit(event) {
     event.preventDefault();
     const { username, password } = this.state
     this.service
-        .login(username, password)
-        .then(response => {
-            const { location } = this.props;
+      .login(username, password)
+      .then(response => {
+        const { location } = this.props;
         this.setState({ username: "", password: "" });
         this.props.getUser(response);
         if (location.state) {
@@ -27,45 +28,49 @@ handleFormSubmit(event) {
         }
       })
       .catch(error => console.log(error));
-  
-    }
 
-    handleChange(event) {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-    }
+  }
 
-    render() {
-        console.log(this.props);
-        return (
-          <div>
-            <form onSubmit={this.handleFormSubmit}>
-              <label>Username:</label>
-              <input
-                type="text"
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="pt-5 card d-flex flex-column justify-content-center">
+        <div className="card-body">
+          <h3 className="card-title">Login</h3>
+          <Form onSubmit={this.handleFormSubmit} className="d-flex flex-column align-items-center">
+            <Form.Group controlId="username" className="col-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text"
                 name="username"
                 value={this.state.username}
-                onChange={this.handleChange}
-              />
-    
-              <label>Password:</label>
-              <input
-                type="password"
+                onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group controlId="password" className="col-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password"
                 name="password"
                 value={this.state.password}
                 onChange={this.handleChange}
               />
+            </Form.Group>
 
-              <input type="submit" value="Login" />
-            </form>
-            {this.state.message && <p>{this.state.message}</p>}
-            <p>
-                Don't have account?
+            <input type="submit" value="Login" />
+          </Form>
+          {this.state.message && <p>{this.state.message}</p>}
+        </div>
+        <p>
+          Don't have account?
               <Link to={"/signup"}> Signup</Link>
-            </p>
-          </div>
-        );
-      }
+        </p>
+      </div>
+    );
+  }
 
 }
 
