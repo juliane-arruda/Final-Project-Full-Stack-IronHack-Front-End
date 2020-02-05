91cdf3d7d05000cb6276807dbf9c9705f3be5945
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-05 03:37:42 -0300

    image and text content has been placed in the home

--- src/components/home/instructions.jsx
+++ src/components/home/instructions.jsx
@@ -3,7 +3,15 @@ import {Parallax} from 'react-parallax';
 
 const Instructions = () => (
   <div>
-
+    <img src="/images/fofo.jpg" alt="background"/>
+    <img src="/images/pets.jpg" alt="pets"/>
+    <div className="container">
+    <h5>como usar nossa ferramenta</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptas quia neque animi consequatur esse aperiam repellat suscipit doloribus, adipisci quos mollitia molestias rem temporibus nihil commodi iusto exercitationem alias?
+    </p>
+    <button>pagina que redenriza foto</button>
+    </div>
   </div>
 );
 
