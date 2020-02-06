467e3d6ae36ed06f7fd1fdf3c26fd8469abfdfc0
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-06 15:31:38 -0300

    corrected responsiveness of the about

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const About = () => (
-  <div className="container-fluid p-5 d-flex ">
-    <div className="container">
-      <img className="w-75 img-fluid " src="/images/about-1.jpg" alt="family" />
+  <div className="container-fluid p-5">
+    <div className="row d-flex align-items-center justify-content-around">
+    <div className="col-12 col-md-6 col-lg-4">
+      <img className="img-fluid img-thumbnail" src="/images/family.jpg" alt="family" />
     </div>
-    <div className="container d-flex flex-column align-items-center justify-content-around p-5">
+    <div className="col-12 col-md-6 col-lg-4">
       <h2>Sobre</h2>
       <p> "Esse conteudo fica no meio da imagem azul"
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, laboriosam reiciendis rem fugiat molestias sit ut officia accusamus minima ipsa voluptatem enim facere. Ipsum totam ex veniam harum possimus fugiat.
     </p>
+    </div>
     </div>
   </div>
 );
