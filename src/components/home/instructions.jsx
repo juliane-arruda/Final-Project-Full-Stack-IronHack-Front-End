f7550447fe933d10e853cfd2ec4bf3dcdf3b9db8
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-06 03:22:24 -0300

    done instruction layout

--- src/components/home/instructions.jsx
+++ src/components/home/instructions.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import {Parallax} from 'react-parallax';
+// import {Parallax} from 'react-parallax';
+import { Link } from 'react-router-dom';
+import './instructions.css';
 
 const Instructions = () => (
-  <div>
-    <img src="/images/fofo.jpg" alt="background"/>
-    <img src="/images/pets.png" alt="pets"/>
-    <div className="container">
-    <h5>como usar nossa ferramenta</h5>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptas quia neque animi consequatur esse aperiam repellat suscipit doloribus, adipisci quos mollitia molestias rem temporibus nihil commodi iusto exercitationem alias?
+  <div className="background-instructions test-0">
+    <h2>Como Usar Nossa Ferramenta</h2>
+    <div className="test-1 container d-flex flex-column justify-content-center">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptas quia neque animi consequatur esse aperiam repellat suscipit doloribus, adipisci quos mollitia molestias rem temporibus nihil commodi iusto exercitationem alias?
     </p>
-    <button>pagina que redenriza foto</button>
+      <div className="container">
+        <Link type="button" class="btn btn-outline-danger m-1" to="/signup-lost" style={{ textDecoration: "none" }}>
+          Perdi Meu Pet
+        </Link>
+        <Link type="button" class="btn btn-outline-success m-1" to="/signup-found" style={{ textDecoration: "none" }}>
+          Encontrei Um Pet
+        </Link>
+      </div>
     </div>
   </div>
 );
