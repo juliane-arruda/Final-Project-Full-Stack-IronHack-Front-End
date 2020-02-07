55adb20ddfe74bf56ec25695493dd0e7c20e4ee5
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-07 02:51:50 -0300

    removed some link and added another

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
     this.onToggle = this.onToggle.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     window.addEventListener('scroll', this.onScroll);
   }
 
@@ -52,42 +52,52 @@ class Navbar extends Component {
     console.log(this.state.loggedInUser);
     const { isHome, userInSession } = this.props;
     const { onTop, isMenuOpen } = this.state;
-    console.log({isMenuOpen})
+    console.log({ isMenuOpen })
 
     return (
       <div className={`Navbar ${isHome ? 'home' : ''} ${onTop ? 'top' : ''} ${isMenuOpen ? 'menu-open' : ''}`}>
         <BtNavbar onToggle={this.onToggle} fixed="top" expand="lg" className="d-flex flex-row p-3 px-lg-4 mb-3 border-bottom shadow-sm">
-        <NavLink className="d-flex align-items-center" to="/">
-        <img src="/images/logo.png" alt="Cat and Dog logo" className="logo mr-3" />
-          <h5 className="my-0 mr-lg-auto font-weight-normal">I Cat Your Pets</h5>
-        </NavLink>
-        
+          <NavLink className="d-flex align-items-center" to="/">
+            <img src="/images/logo.png" alt="Cat and Dog logo" className="logo mr-3" />
+            <h5 className="my-0 mr-lg-auto font-weight-normal">I Cat Your Pets</h5>
+          </NavLink>
+
           <BtNavbar.Toggle aria-controls="basic-navbar-nav" />
           <BtNavbar.Collapse onAuxClick={this.menuClick} className="d-lg-flex flex-row justify-content-end" id="basic-navbar-nav">
             <nav className="d-flex flex-column flex-lg-row align-content-center my-2 my-lg-0 mr-lg-3" id="menu">
               {userInSession && (
                 <>
-                  <NavLink className="p-2 text-dark" activeStyle={{ color: "red" }} to="/pets">
+                  <NavLink className="p-2 text-dark" activeStyle={{ color: "blue" }} to="/pets" style={{ textDecoration: "none" }}>
                     Pets
                   </NavLink>
-                  <NavLink className="p-2 text-dark" to="/signup-found" style={{ textDecoration: "none" }}>
+                  {/* <NavLink className="p-2 text-dark" to="/signup-found" style={{ textDecoration: "none" }}>
                     Encontrei um pet
-                  </NavLink>
-                  <NavLink className="p-2 text-dark" to="/signup-lost" style={{ textDecoration: "none" }}>
+                  </NavLink> */}
+                  {/* <NavLink className="p-2 text-dark" to="/signup-lost" style={{ textDecoration: "none" }}>
                     Perdi meu pet
-                  </NavLink>
-                  <NavLink className="p-2 text-dark" activeStyle={{ color: "yellow" }} to="/map" style={{ textDecoration: "none" }}>
-                    Mapa
-                  </NavLink>
+                  </NavLink> */}
                 </>
               )}
               {!userInSession && (
                 <>
-                  <HashLink to="#about" className="p-2 text-dark">Sobre</HashLink>
-                  <HashLink to="/#howItWorks" className="p-2 text-dark">Como funciona?</HashLink>
-                  <HashLink to="/#instructions" className="p-2 text-dark">Instruções</HashLink>
-                  <HashLink to="/#depositions" className="p-2 text-dark">Depoimentos</HashLink>
-                  <HashLink to="/#contact" className="p-2 text-dark">Contato</HashLink>
+                  <HashLink to="/#about" className="p-2 text-dark" activeStyle={{ color: "blue" }} style={{ textDecoration: "none" }}>
+                    Sobre
+                    </HashLink>
+                  <HashLink to="/#howItWorks" className="p-2 text-dark" activeStyle={{ color: "blue" }} style={{ textDecoration: "none" }}>
+                    Como funciona
+                    </HashLink>
+                  <HashLink to="/#instructions" className="p-2 text-dark" activeStyle={{ color: "blue" }} style={{ textDecoration: "none" }}>
+                    Instruções
+                    </HashLink>
+                  <HashLink to="/#depositions" className="p-2 text-dark" activeStyle={{ color: "blue" }} style={{ textDecoration: "none" }}>
+                    Depoimentos
+                    </HashLink>
+                  <HashLink to="/#contact" className="p-2 text-dark" activeStyle={{ color: "blue" }} style={{ textDecoration: "none" }}>
+                    Contato
+                    </HashLink>
+                  <NavLink className="p-2 text-dark" activeStyle={{ color: "blue" }} to="/map" style={{ textDecoration: "none" }}>
+                    Mapa
+                  </NavLink>
                   {/* <NavLink className="p-2 text-dark" to="/signup-found">
                     Encontrei um pet
                   </NavLink>
@@ -98,18 +108,13 @@ class Navbar extends Component {
               )}
             </nav>
             {!userInSession && (
-              <>
-                <NavLink className="btn btn-outline-primary mr-2" to="/login">
-                  Login
-                </NavLink>
-                <NavLink className="btn btn-outline-primary" to="/signup">
-                  Criar conta
-                </NavLink>
-              </>
+              <NavLink className="btn btn-outline-primary mr-2" to="/login">
+                Login
+              </NavLink>
             )}
             {userInSession && (
               <NavLink className="btn btn-outline-danger" onClick={() => this.logoutUser()} to="/" exact>
-                  Logout
+                Logout
               </NavLink>
             )}
           </BtNavbar.Collapse>
