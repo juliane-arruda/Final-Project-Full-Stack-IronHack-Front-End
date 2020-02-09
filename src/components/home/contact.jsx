ad167f93283a4da505644df51982665fbd7f176e
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-09 20:39:08 -0300

    to organize the map size proportions in the house

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => (
         <img className="img-fluid btn-social-icon btn-instagram p-1 m-1" src="/images/icons/instagram.png" alt="instagram" />
       </div>
       <div className="col-12 col-md-6 col-lg-4">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6255.526157490973!2d-46.660939413709485!3d-23.561946062049994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59468d48985b%3A0x694f27fed85d9b0c!2sIronhack%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1580927864945!5m2!1spt-BR!2sbr" width={"350"} height={"250"} frameborder={"0"} border={"0"} allowfullscreen={""}></iframe>
+        <iframe className="home-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6255.526157490973!2d-46.660939413709485!3d-23.561946062049994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59468d48985b%3A0x694f27fed85d9b0c!2sIronhack%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1580927864945!5m2!1spt-BR!2sbr" frameborder={"0"} border={"0"} allowfullscreen={""}></iframe>
       </div>
     </div>
   </div>
