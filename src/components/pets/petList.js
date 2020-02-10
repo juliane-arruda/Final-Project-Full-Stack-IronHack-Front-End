1a34a321a44f8fe429760ad8028ccc94b1418b1c
Juliane Arruda <juliane_ch.arruda@live.com>
2020-02-09 22:35:30 -0300

    added responsiveness

--- src/components/pets/petList.js
+++ src/components/pets/petList.js
@@ -27,22 +27,20 @@ class PetList extends Component {
   render() {
     return (
       <div className="PetList container-fluid container-details">
-        <div className="row p-5 d-flex align-items-start justify-content-center">
-          {this.state.listOfPets && this.state.listOfPets.map(pet => {
-            return (
-
-          <div className="image mt-3 col-12 col-sm-6 col-lg-4" id={pet._id} key={pet._id}>
-            <img src={pet.imageUrl} className="card-img-top img-fluid"/>
-            <Link to={`/pets/${pet._id}`} className="details">
-              <h2  className="">{pet.petName}</h2>
-              <p className="">{pet.role}</p>
-            </Link>
-          </div>
-            );
-          })}
+        <div className="row p-5 d-flex align-items-stretch justify-content-center">
+          {this.state.listOfPets && this.state.listOfPets.map(pet => (
+            <div className="image mt-3 col-12 col-sm-6 col-lg-4" id={pet._id} key={pet._id}>
+              <div className="card w-100 h-100 d-flex flex-column justify-content-center">
+                <img src={pet.imageUrl} />
+              </div>
+              <Link to={`/pets/${pet._id}`} className="details">
+                <h2  className="">{pet.petName}</h2>
+                <p className="">{pet.role}</p>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
-
     );
   }
 }
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,58 +6,46 @@ import AuthService from '../components/auth/auth-service';
 
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-      
-  
-      }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-    render() {
-      console.log('props', this.props)
-        return(
-          
-          <>
-          { this.props.user && this.props.pets && this.props.newPet ? (
-            <div className=" container-details p-5">
-              <div className="my-pet">
-              
-              <div>
-            <img className="my-img" src={this.props.newPet.imageUrl}/>
-            <h1> {this.props.newPet.petName} </h1> 
-              </div>
-
-              </div>
-              <div className=""></div>
-            <div className="row row-cols-1 row-cols-md-3 m-3 d-flex justify-content-center" >
-              {this.props.pets.length > 0 ? this.props.pets.map((el) => (
-          
-          <div className="imagem m-2">
+  render() {
+    return (
+      <>
+        {this.props.user && this.props.pets && this.props.newPet ? (
+          <div className="container-details p-5">
+            <div className="row">
+              <div className="col-12 col-md-6">
                 <div>
-                  <div>
-                    <img src={el.imageUrl} className="card-img-top"/>
-                  </div>
-                    {/* <div className="col-md-8"> */}
-                      <div>
-                    
-                  <Link to={`/pets/${el._id}`} className="capa">
-                  <h2>{el.petName}</h2>
-                    </Link>
+                  <img className="my-img" src={this.props.newPet.imageUrl} />
+                  <h1> {this.props.newPet.petName} </h1>
                 </div>
-                {/* </div> */}
               </div>
-        </div>
-)) : <h3 className="alert alert-warning">Nenhum pet parecido foi encontrado, assim que ele for reconhecido, você será notificado.</h3>
-
-              }
+              <div className="col-12 col-md-6 mt-3 mt-md-0 d-flex justify-content-center align-items-start">
+                {this.props.pets.length > 0 ? (
+                  <div className="row d-flex align-items-stretch">
+                    {this.props.pets.map((el) => (
+                      <div className="col-12 col-md-6 mb-3">
+                        <div className="card h-100 w-100 d-flex flex-column justify-content-center">
+                          <img src={el.imageUrl} className="img-fluid" />
+                          <Link to={`/pets/${el._id}`} className="capa">
+                            <h2>{el.petName}</h2>
+                          </Link>
+                        </div>
+                      </div>
+                    ))}
+                  </div>) : (
+                  <h3 className="alert alert-info">Nenhum pet parecido foi encontrado, assim que ele for reconhecido, você será notificado.</h3>
+                )}
               </div>
-
             </div>
-
-          ): <div className="spinner-border text-primary m-5"></div>}
-          </>
-        )
-    }
+          </div>
+        ) : <div className="spinner-border text-primary m-5"></div>}
+      </>
+    )
+  }
 }
 
 export default Search;
